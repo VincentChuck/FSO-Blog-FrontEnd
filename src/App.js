8e954924ea20e7905d665573720e6cd1aa4964af
Vincent Chuck <vincent.chuck7@gmail.com>
2022-11-12 14:38:43 +0800

    Commit exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,80 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    // try {
+    const user = await loginService.login({
+      username, password,
+    })
+    setUser(user)
+    setUsername('')
+    setPassword('')
+    // } catch (exception) {
+    //   setErrorMessage('Wrong Credentials')
+    //   setTimeout(() => {
+    //     setErrorMessage(null)
+    //   }, 5000)
+    // }
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
 
-  return (
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+          username
+        <input
+          type='text'
+          value={username}
+          name='Username'
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+          password
+        <input
+          type='password'
+          value={password}
+          name='Password'
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  )
+
+  const blogDisplay = () => (
     <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged-in</p>
+          {blogDisplay()}
+        </div>
+      }
     </div>
   )
 }
