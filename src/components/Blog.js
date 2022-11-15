95ed0b8daf5f0d3c9d77bb15bf3c1a987e068d68
Vincent Chuck <vincent.chuck7@gmail.com>
2022-11-15 17:32:09 +0800

    Commit exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setBlogVisible(!blogVisible)
+  }
+
+  return (
+    <div style={blogStyle}>
+
+      <div style={hideWhenVisible}>
+        {blog.title} &nbsp;
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+
+      <div style={showWhenVisible}>
+        {blog.title} <button onClick={toggleVisibility}>hide</button> <br/>
+        {blog.url} <br/>
+        likes {blog.likes} <button>like</button> <br/>
+        {blog.author}
+      </div>
+
+    </div>
+  )
+}
 
 export default Blog
