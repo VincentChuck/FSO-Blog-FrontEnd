13a083878d4d13ec8a29785139d36025d388205c
Vincent Chuck <vincent.chuck7@gmail.com>
2022-11-16 04:45:56 +0800

    Commit exercise 5.9

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ const App = () => {
   const blogDisplay = () => (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.sort((a,b) => b.likes - a.likes ).map(blog =>
         <Blog key={blog.id} blog={blog} addLike={addLike} />
       )}
     </div>
