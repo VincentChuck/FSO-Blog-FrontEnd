26cd15d1b9615a303bd95bfecceefdfcd0ab8074
Vincent Chuck <vincent.chuck7@gmail.com>
2022-11-18 00:08:27 +0800

    Commit exercise 5.15

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -54,3 +54,19 @@ describe('each blog', () => {
     expect(expandedView).toHaveTextContent(blog.likes)
   })
 })
+
+describe('like button', () => {
+  test('calls event handler twice when pressed twice', async () => {
+    const mockHandler = jest.fn()
+    render(
+      <Blog blog={blog} addLike={mockHandler} />
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('like')
+    await user.click(button)
+    expect(mockHandler.mock.calls).toHaveLength(1)
+    await user.click(button)
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
+})
